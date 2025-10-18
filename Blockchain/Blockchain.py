923b5d754e559e0f8d86d996a74f80ccda93d323
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2025-10-18 13:17:13 +0330

    Blockchain Created and finished

--- Blockchain/Blockchain.py
+++ Blockchain/Blockchain.py
@@ -61,4 +61,37 @@ app = Flask(__name__)
 blockchain = Blockchain()
 
 #Mining a new Block
-@app.route('/mine_block')
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'New Block Forged',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+#Get full Blockchain
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+
+#Is chain Valid
+@app.route('/is_valid', methods=['GET'])
+def check_validity():
+    valid = blockchain.is_chain_valid(blockchain.chain)
+    if not valid:
+        response = {'message': 'Block is not valid'}
+        return jsonify(response), 400
+    response = {'message': 'Block is valid'}
+    return jsonify(response), 200
+
+#Running the APP
+app.run(host='0.0.0.0', port=5000)
