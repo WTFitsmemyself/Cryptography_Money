04d6640aef35f1d1f88a23b2d828e658665150f5
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2025-10-23 16:57:18 +0330

    Module of Coins updated

--- Cryptocurrency/Bitcoin.py
+++ Cryptocurrency/Bitcoin.py
@@ -12,6 +12,7 @@ class Blockchain:
         self.chain = []
         self.transactions = []
         self.create_block(proof = 1, previous_hash = '0')
+        self.nodes = set()
 
     def create_block(self, proof, previous_hash):
         block = {
@@ -67,6 +68,25 @@ class Blockchain:
         })
         previous_block = self.get_previous_block()
         return previous_block['index'] + 1
+
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def replace_chain(self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        for node in network:
+            response = requests.get(f'http:/get_chain/{node}/')
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length > max_length and self.is_chain_valid(chain):
+                    max_length = length
+                    longest_chain = chain
+
+
 
 #Part 2 - Mining our Blockchain
 #Creating Web App
